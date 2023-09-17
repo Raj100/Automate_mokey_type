fa077bf235ba66337285e411c6465ddbc616c5e1
Raj Rawariya <98628762+Raj100@users.noreply.github.com>
2023-09-18 00:07:17 +0530

    commit #6

--- main.py
+++ main.py
@@ -7,30 +7,32 @@ os.environ['PATH'] += r"/Users/raj/Documents/Python/chromedriver_mac64"
 driver = webdriver.Chrome()
 driver.get("https://monkeytype.com")
 driver.maximize_window()
-driver.implicitly_wait(2)
 time.sleep(2)
+driver.implicitly_wait(2)
+reject = driver.find_element(By.CLASS_NAME, "rejectAll")
+reject.click()
 words = driver.find_element(By.ID, "typingTest")
 l = []
 l = words.text
 l = l.replace("english", "", 1)
 l = l.split()
-nl=len(l)-1
+nl = len(l) - 1
 def convert(l):
     return ' '.join(l)
-t_end=time.time()+30
+t_end = time.time() + 30
 while t_end >= time.time():
-    w=convert(l)
+    w = convert(l)
     pyautogui.typewrite(w)
     pyautogui.typewrite(" ")
     words = driver.find_element(By.ID, "typingTest")
     a = words.text
     a = a.split()
-    n = len(a)-1
+    n = len(a) - 1
     j = 0
-    while  n-j>=0 and n-j-1>=0 and n - j - 2>=0 and nl-2>=0:
-        if a[n - j] == l[nl] and a[n - j - 1] == l[nl - 1]and a[n - j - 2] == l[nl - 2]:
+    while n - j >= 0 and n - j - 1 >= 0 and n - j - 2 >= 0 and nl - 2 >= 0:
+        if a[n - j] == l[nl] and a[n - j - 1] == l[nl - 1] and a[n - j - 2] == l[nl - 2]:
             break
-        j+= 1
+        j += 1
     l = a[n - j + 1:]
     nl = len(l) - 1
 time.sleep(10)
